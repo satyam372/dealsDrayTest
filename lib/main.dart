3542c086fe172399ecb2f56dc53e22cc5e834a52
satyam_45 <satyampokharna45@gmail.com>
2024-10-19 16:02:03 +0530

    main[nfc]: Add const modifier to improve performance

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:deals_dray_test/View/Splash.dart';
+import 'package:deals_dray_test/View/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:SplashScreen(),
+      home:const SplashScreen(),
     );
   }
 }
